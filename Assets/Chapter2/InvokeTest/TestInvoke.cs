1296af8410bfa8508d96fe9c35f778f6bf2052d4
NoraLands <nuoyang033@gmail.com>
2022-03-17 21:55:23 +0800

    [Chapter2][Add]InvokeTest

---
+++ Assets/Chapter2/InvokeTest/TestInvoke.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class TestInvoke : MonoBehaviour
+{
+    public GameObject prefab;
+    int Counter = 0;
+    void Start()
+    {
+        Invoke("CreatePrefab", 0.5f);
+        Debug.Log("Invoke 1 is been called");
+    }
+
+    void CreatePrefab(){
+        Vector3 pos = new Vector3(  Mathf.Cos(Counter * 2 * Mathf.PI) / 10, 0, Mathf.Sin(Counter * 2 * Mathf.PI) / 10);
+        pos *= 5;
+        Instantiate(prefab, pos, Quaternion.identity);
+        Counter++;
+        if (Counter < 10){
+            Invoke("CreatePrefab", 0.5f);
+            Debug.Log("Invoke 1 is been called" + Counter);
+        }
+    }
+}
